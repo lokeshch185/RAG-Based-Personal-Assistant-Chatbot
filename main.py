3ee0b4bba19eaf31c60d6981054e3f04bb8dcb11
lokesh chaudhari <lokeshchaudhari14@gmail.com>
2025-01-16 00:44:21 +0530

    added embedding and vector store

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 
 file_path = "./profile.pdf"
@@ -15,9 +17,18 @@ print(docs[0].metadata)
 
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
+    chunk_size=150, chunk_overlap=50, add_start_index=True
 )
 all_splits = text_splitter.split_documents(docs)
 
 print(len(all_splits))
 
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+vector_store = FAISS.from_documents(all_splits, embeddings)
+ids = vector_store.add_documents(documents=all_splits)
+results = vector_store.similarity_search(
+    "query"
+)
+
+print(results[0])
